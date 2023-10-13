758fbfec77a1137d76da5044ec8a0f890a4867ef
Sogon-Njugush <josephnjuguna00@gmail.com>
2023-10-13 18:44:41 +0300

    Adding react dom Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import {BrowserRouter,Routes, Route} from "react-router-dom"
+import Home from './pages/About';
+import About from './pages/About';
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+import Profile from './pages/Profile';
 
 export default function App() {
   return (
-    <div>
-      <h2 className='text-3xl font-bold underline'> Cardin Wanjiru njogu</h2>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}></Route>
+      <Route path="/about" element={<About/>}></Route>
+      <Route path='/sign-up' element={<SignUp/>}></Route>
+      <Route path='/sign-in' element={<SignIn/>}></Route>
+      <Route path='/profile' element={<Profile/>}></Route>
+    </Routes>
+    </BrowserRouter>
   )
 }
