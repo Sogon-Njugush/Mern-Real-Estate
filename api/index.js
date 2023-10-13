f2a628f3743fa5a4d906b3d18729a85b478095a9
Sogon-Njugush <josephnjuguna00@gmail.com>
2023-10-13 22:26:34 +0300

    create user model

--- api/index.js
+++ api/index.js
@@ -1,7 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then( ()=>{
+    console.log('Connected to MongoDB !');
+}).catch((err) =>{
+  console.log(err);
+});
 
 
-const app = express ();
+const app = express (); 
 
 
 app.listen(3000,() =>{
@@ -9,3 +18,4 @@ app.listen(3000,() =>{
 }
 
 );
+
